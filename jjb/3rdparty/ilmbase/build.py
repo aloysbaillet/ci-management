2cb34116a75c396e57a616d41a566e2c01012847
Aloys Baillet <aloys.baillet@gmail.com>
2019-01-02 23:19:09 +1100

    WIP with published build image

--- jjb/3rdparty/ilmbase/build.py
+++ jjb/3rdparty/ilmbase/build.py
@@ -1,10 +1,11 @@
 import subprocess
 
 if __name__ == "__main__":
-    subprocess.check_call('docker build -t aswf/vfx2018-builder -f Dockerfile_vfx2018 .', shell=True, cwd='../../../conan')
+    #subprocess.check_call('docker build -t aloysbaillet/aswf-vfx2018-conan -f Dockerfile_vfx2018 .', shell=True, cwd='../../../conan')
 
     build = '''
+conan user demo -p demo -r=local_server
 conan create /tmp/vfx-build/ilmbase aswf/vfx2018 &&\
-conan upload IlmBase/2.2.0@aswf/vfx2018 --all -r=my_local_server
+conan upload IlmBase/2.2.0@aswf/vfx2018 --all -r=local_server
 '''
-    subprocess.check_call('docker run -v $(pwd):/tmp/vfx-build/ilmbase --rm aswf/vfx2018-builder /bin/bash -c "%s"'%build, shell=True)
+    subprocess.check_call('docker run --network="host" -v $(pwd):/tmp/vfx-build/ilmbase --rm aloysbaillet/aswf-vfx2018-conan /bin/bash -c "%s"'%build, shell=True)
--- jjb/3rdparty/openexr/build.py
+++ jjb/3rdparty/openexr/build.py
@@ -1,10 +1,11 @@
 import subprocess
 
 if __name__ == "__main__":
-    subprocess.check_call('docker build -t aswf/vfx2018-builder -f Dockerfile_vfx2018 .', shell=True, cwd='../../../conan')
+    #subprocess.check_call('docker build -t aloysbaillet/aswf-vfx2018-conan -f Dockerfile_vfx2018 .', shell=True, cwd='../../../conan')
 
     build = '''
+conan user demo -p demo -r=local_server
 conan create /tmp/vfx-build/openexr aswf/vfx2018 &&\
-conan upload OpenEXR/2.2.0@aswf/vfx2018 --all -r=my_local_server
+conan upload OpenEXR/2.2.0@aswf/vfx2018 --all -r=local_server
 '''
-    subprocess.check_call('docker run -v $(pwd):/tmp/vfx-build/openexr --rm aswf/vfx2018-builder /bin/bash -c "%s"'%build, shell=True)
+    subprocess.check_call('docker run --network="host" -v $(pwd):/tmp/vfx-build/openexr --rm aloysbaillet/aswf-vfx2018-conan /bin/bash -c "%s"'%build, shell=True)
