de9efbfb9e33e30065d99e064b5fe77472c5e4be
aloysb <aloysb@al.com.au>
2019-01-03 18:17:50 +1100

    working build script with local conan server

--- jjb/3rdparty/README.md
+++ jjb/3rdparty/README.md
@@ -1,36 +1,12 @@
 # Third Party Packages
 
-This set of jenkins jobs build binaries for VFX Platform packages that are required to build ASWF packages.
+This set of jenkins jobs to build binaries for VFX Platform packages that are required to build ASWF packages.
 
 All builds are based on a minimal VFX Platform compliant docker image.
 
-## Package Layout
+## Conan
 
-The layout of installed packages looks like this:
-```
-/opt/aswf/
-`-- vfx2018
-    |-- boost
-    |   |-- include
-    |   `-- lib
-    |-- ilmbase
-    |   |-- include
-    |   `-- lib
-    |-- openexr
-    |   |-- bin
-    |   |-- include
-    |   |-- lib
-    |   `-- share
-    |-- pyilmbase
-    |   |-- include
-    |   |-- lib
-    |   |-- lib64
-    |   `-- share
-    `-- tbb
-        |-- include
-        `-- lib
-```
-When building or using these packages the `LD_LIBRARY_PATH` and `PYTHONPATH` environment variables must be set accordingly, see the `all/Dockerfile` file for an example.
+This is using [conan.io] to build and retrieve dependencies.
 
 ## Jenkins Jobs
 
@@ -44,6 +20,8 @@ Use the regular `jenkins-jobs update jjb/` script to upload the jobs to the jenk
 In order to test these docker builds locally the `build_local.py` script can be used to build all 3rdparty packages in the right order
 and with the right dependencies, the only requirement is a linux machine with docker installed.
 
-When running locally, an extra docker image is built called `all` that brings in all previously built packages. This allows local inspection of 
-the built artifacts in a working environment and easier debugging.
-To inspect the all-builder image run this `docker run -it --rm --entrypoint bash all-builder`.
+To build all using a local conan server (running within docker as well):
+`python build_local.py -r local_server`
+
+To build a single package using a local conan server (running within docker as well):
+`python build_local.py -r local_server OpenEXR`
--- jjb/3rdparty/build_local.py
+++ jjb/3rdparty/build_local.py
@@ -1,44 +1,81 @@
-#!/usr/bin/env python
+import subprocess
+import argparse
 import sys
 import os
-import shutil
-import subprocess
-import glob
-import collections
+from contextlib import contextmanager
 
-PACKAGES = collections.OrderedDict([('boost', []),
-                                    ('tbb', []),
-                                    ('ilmbase', []),
-                                    ('pyilmbase', ['boost', 'ilmbase']),
-                                    ('openexr', ['ilmbase'])])
-PACKAGES['all'] = PACKAGES.keys()
+PACKAGES = ['IlmBase',
+            'OpenEXR']
 
-PACKAGE_FILES = {}
+def host_call(cmd):
+    print('+ %s'%cmd)
+    return subprocess.check_call(cmd, shell=True)
 
-def main(args):
-    curdir = os.path.abspath(os.curdir)
-    workspace = os.path.dirname(os.path.dirname(curdir))
+def docker_call(cmd, cmds):
+    cmds.append(cmd)
 
-    print('Building all in workspace %s'%workspace)
-    os.environ['WORKSPACE'] = workspace
+@contextmanager
+def managed_docker(args):
+    if args.remote == 'local_server':
+        repoRoot = os.path.abspath(os.path.join(os.path.curdir, '..', '..'))
+        host_call('docker network create --driver bridge conan_network')
+        host_call('docker run '
+                  '--network=conan_network '
+                  '--detach -p 9300:9300 '
+                  '-v %s/conan/server:/root/.conan_server '
+                  '--name local_conan_server '
+                  'conanio/conan_server:latest'%repoRoot)
+    try:
+        yield None
+    finally:
+        if args.remote == 'local_server':
+            host_call('docker stop local_conan_server')
+            host_call('docker rm --force local_conan_server')
+            host_call('docker network rm conan_network')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Build script')
+    parser.add_argument('--no-ssl-verify', '-ns', action='store_true', help='Disable SSL cert verify on remotes.')
+    parser.add_argument('--upload', '-up', action='store_true', help='Upload package after successful build.')
+    parser.add_argument('--remote', '-r', help='Remote name for upload. Use "local_server" to start a local conan server. '
+                        'No need for username/password with local_server.')
+    parser.add_argument('--username', '-u', help='Username for upload to remote.')
+    parser.add_argument('--password', '-p', help='Password or API Key for upload to remote.')
+    parser.add_argument('packages', nargs='+', help='Package names to build, builds all if not specified')
+    args = parser.parse_args()
+
+    cmds = []
+    if args.remote == 'local_server':
+        docker_call('conan remote update local_server http://local_conan_server:9300 False', cmds)
+        if args.remote == 'local_server':
+            docker_call('conan user local_user -p local_password -r=local_server', cmds)
+        else:
+            docker_call('conan user %s -p %s -r=%s'%(args.username, args.password, args.remote), cmds)
+
+    if args.no_ssl_verify:
+        docker_call('conan remote update conan-center https://conan.bintray.com False', cmds)
+
+    docker_call('conan remote add aswftest https://api.bintray.com/conan/aloysbaillet/aswftest %r'%(not args.no_ssl_verify), cmds)
+
+    for pkg in args.packages or PACKAGES:
+        docker_call('conan create /tmp/vfx-build/%s aswf/vfx2018'%pkg, cmds)
     
-    print('Downloading...')
-    for pkg in PACKAGES.keys():
-        subprocess.check_call('%s/download.sh'%pkg)
+        if args.upload:
+            docker_call('conan upload %s/*@aswf/vfx2018 --force --confirm --all -r=%s'%(pkg, args.remote), cmds)
 
-    for pkg, deps in PACKAGES.iteritems():
-        print('Building %s...'%pkg)
-        for dep in deps:
-            dst = os.path.join(pkg, os.path.basename(PACKAGE_FILES[dep]))
-            if not os.path.exists(dst):
-                print('Copying {} -> {}'.format(PACKAGE_FILES[dep], dst))
-                shutil.copyfile(PACKAGE_FILES[dep], dst)
-        cwd = os.path.join(curdir, pkg)
-        subprocess.check_call('docker build -t {pkg}-builder .'.format(pkg=pkg), cwd=cwd, shell=True)
-        subprocess.check_call('docker run --name {pkg}-builder --rm -v {cwd}:/mnt/dist {pkg}-builder'.format(pkg=pkg, cwd=cwd), cwd=cwd, shell=True)
-        pkgFile = glob.glob('%s/%s*vfx-2018.tar.bz2'%(pkg, pkg))[0]
-        print('Built artifact: {}'.format(pkgFile))
-        PACKAGE_FILES[pkg] = pkgFile
+    with managed_docker(args):
+        print('Docker calls:\n' + '\n- '.join(cmds))
+        build = ' && '.join(cmds)
+        host_call('docker run '
+                  '--network=conan_network '
+                  '-v $(pwd):/tmp/vfx-build '
+                  '--rm '
+                  'aloysbaillet/aswf-vfx2018-conan '
+                  '/bin/bash -c "%s"'%build)
 
-if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
